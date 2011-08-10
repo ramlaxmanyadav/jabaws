e02dbe64b92e07f2a25864e39b8e12ebc71dc323
to.petr@gmail.com <to.petr@gmail.com@c5384192-035d-1509-ae0d-e91ba3b9d0b1>
2011-08-10 10:54:38 +0000

    AAProperties wrapper

--- trunk/jabaws/runner/compbio/runner/sequence/AAProperties.java
+++ trunk/jabaws/runner/compbio/runner/sequence/AAProperties.java
@@ -1,25 +1,24 @@
 package compbio.runner.sequence;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 
 import org.apache.log4j.Logger;
 
 import compbio.data.sequence.ScoreManager;
 import compbio.data.sequence.SequenceUtil;
-import compbio.data.sequence.UnknownFileFormatException;
 import compbio.engine.client.SkeletalExecutable;
 import compbio.metadata.ResultNotAvailableException;
 
-public class AAProperties extends SkeletalExecutable<AAProperties>{
+public class AAProperties extends SkeletalExecutable<AAProperties> {
 	private static Logger log = Logger.getLogger(AAProperties.class);
-	
-	public AAProperties(){
-		addParameters(Arrays.asList("-jar"));
+
+	public AAProperties() {
+		addParameters(Arrays.asList("-jar", getLibPath(), "-a"));
 	}
 
 	@SuppressWarnings("unchecked")
@@ -28,18 +27,14 @@ public class AAProperties extends SkeletalExecutable<AAProperties>{
 			throws ResultNotAvailableException {
 		ScoreManager sequences = null;
 		try {
-			InputStream inStream = new FileInputStream(new File(workDirectory,
-					getOutput()));
-			sequences = ScoreManager.newInstanceSingleScore(SequenceUtil
-					.readJRonn(inStream));
-			inStream.close();
+			BufferedReader reader = new BufferedReader(new FileReader(new File(
+					workDirectory, getOutput())));
+			sequences = SequenceUtil.parseAAProp(reader);
+			reader.close();
 		} catch (FileNotFoundException e) {
 			log.error(e.getMessage(), e.getCause());
 			throw new ResultNotAvailableException(e);
 		} catch (IOException e) {
-			log.error(e.getMessage(), e.getCause());
-			throw new ResultNotAvailableException(e);
-		} catch (UnknownFileFormatException e) {
 			log.error(e.getMessage(), e.getCause());
 			throw new ResultNotAvailableException(e);
 		} catch (NullPointerException e) {
@@ -47,6 +42,34 @@ public class AAProperties extends SkeletalExecutable<AAProperties>{
 			throw new ResultNotAvailableException(e);
 		}
 		return sequences;
+	}
+	@Override
+	public AAProperties setOutput(String outFile) {
+		super.setOutput(outFile);
+		cbuilder.setParam("-o", outFile);
+		return this;
+	}
+
+	@Override
+	public AAProperties setInput(String inFile) {
+		super.setInput(inFile);
+		cbuilder.setParam("-i", inFile);
+		return this;
+	}
+
+	private static String getLibPath() {
+		String settings = ph.getProperty("aaproperties.jar.file");
+		if (compbio.util.Util.isEmpty(settings)) {
+			throw new NullPointerException(
+					"Please define aaproperties.jar.file property in Executable.properties file"
+							+ "and initialize it with the location of jronn jar file");
+		}
+		if (new File(settings).isAbsolute()) {
+			// the jar can be found so no actions necessary
+			// no further actions is necessary
+			return settings;
+		}
+		return compbio.engine.client.Util.convertToAbsolute(settings);
 	}
 
 	@SuppressWarnings("unchecked")
